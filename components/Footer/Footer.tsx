bbc923bfd042f58bf3659814f58e5876409176f3
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-08-31 18:36:21 +0200

    add initial footer component

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+function Footer() {
+    return (
+        <div id="footer">
+            I'm a footer indeed
+        </div>
+    )
+}
+
+export default Footer;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import CustomRouter from "../components/CustomRouter";
 import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <CustomRouter />
+      <Footer />
     </>
   )
 }
