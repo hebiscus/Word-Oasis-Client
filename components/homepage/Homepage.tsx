b3afd38b25dc070393472d2e806ca189b4b8348a
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-08-31 21:10:27 +0200

    attempt at fetch posts, failing as of now

--- components/homepage/Homepage.tsx
+++ components/homepage/Homepage.tsx
@@ -1,7 +1,48 @@
+import { useState, useEffect } from "react";
 
 function Homepage() {
+    const [mainPost, setMainPost] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        async function getPosts() {
+            setIsLoading(true);
+            try {
+                const post = await fetch("https://word-oasis-api-production.up.railway.app/posts");
+                const extracted = await post.json();
+                setIsLoading(false);
+                if (!ignore) {
+                    setMainPost(extracted);
+                    console.log(extracted)
+                  }
+            } catch(err) {
+                console.log(err);
+            }
+        }
+
+        let ignore = false;
+
+        getPosts();
+        
+        return () => {
+            ignore = true;
+        }
+    }, [])
+
     return (
-        <div>You're on a homepage</div>
+        <>
+        {/* {isLoading ? <h3>Data is loading</h3> : mainPost.blogPosts.map((post) => {
+            console.log(post)
+            return <div></div>
+        })} */}
+        <div>
+            <div className="image-placeholder"></div>
+            <h2></h2>
+            <p></p>
+            <button>Read more</button>
+        </div>
+        <div></div>
+        </>
     )
 }
 
