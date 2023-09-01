59a95c58b862cd6e444f658d39ffbf3c521e70fe
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-09-01 20:41:00 +0200

    make interfaces folder along with blogPost interface

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,9 @@
+export interface blogPostInterface{
+    author: string,
+    title: string,
+    content: string,
+    creationDate: string,
+    status: string,
+    _v: number,
+    _id: string
+}
