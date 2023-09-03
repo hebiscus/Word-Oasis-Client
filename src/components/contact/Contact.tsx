8611a972ffdba42b8aac1ece14aae75815c9dfe5
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-09-03 20:09:59 +0200

    make default contact page, add logos

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -2,7 +2,25 @@
 
 function Contact() {
     return (
-        <div>contact page</div>
+        <>
+            <p>
+                If you'd like to reach out, whether it be work or just casual chat related, I'm available on several platforms:
+            </p>
+            <div className="contact-links">
+                <div className="github-link-box">
+                    <img src="../github-logo.png" alt="discord logo"/>
+                    <a>Github: https://github.com/hebiscus</a>
+                </div>
+                <div className="linkedin-link-box">
+                    <img src="../linkedin-logo.png" />
+                    <a>Linkedin: https://www.linkedin.com/in/katarzyna-gasiorek/</a>
+                </div>
+                <div className="discord-link-box">
+                    <img src="../discord-logo.png" />
+                    <span>Discord: hebiscus.</span>
+                </div>
+            </div>
+        </>
     )
 }
 
