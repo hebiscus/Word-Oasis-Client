26b21e3decbeef7a17770ce11eb78d58e4817fa1
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-09-18 10:54:21 +0200

    change all posts page to display a message when no posts available

--- src/components/blogposts/BlogPosts.tsx
+++ src/components/blogposts/BlogPosts.tsx
@@ -5,7 +5,8 @@ import { Link } from "react-router-dom";
 import DefaultButton from "../defButton";
 
 function BlogPosts() {
-    const [allPosts, setAllPosts] = useState<blogPostInterface[] | null>(null)
+    const [allPosts, setAllPosts] = useState<blogPostInterface[] | null>(null);
+    const [allPostsMessage, setAllPostsMessage] = useState("");
 
     useEffect(() => {
         async function getPosts() {
@@ -13,7 +14,8 @@ function BlogPosts() {
                 const allPostsFetch = await fetch("https://word-oasis-api-production.up.railway.app/posts?sorting=descending", {mode: "cors"});
                 const allPosts = await allPostsFetch.json();
                 if (!ignore) {
-                    setAllPosts(allPosts);
+                    setAllPosts(allPosts.foundPosts);
+                    setAllPostsMessage(allPosts.message);
                     console.log(allPosts);
                 }
             } catch(err) {
@@ -32,19 +34,21 @@ function BlogPosts() {
 
     return (
         <>
-        {allPosts && 
-            <div className="allPosts-container">
-                {allPosts.map((post) => {
-                    return <div className="post-container" key={post._id}>
-                            {post.imageURL ? <img src={post.imageURL} /> : <div className="image-placeholder2"/>}
-                            <h3>{post.title}</h3>
-                            <p>{`${post.content.slice(0, 150)}...`}</p>
-                            <Link to={post._id}>
-                                <DefaultButton btnType="button">Read more</DefaultButton>
-                            </Link>
-                    </div>
-                })}
-            </div>}
+        {allPosts && <div className="allPosts-container">
+            {allPosts.length === 0
+            ? <div>{allPostsMessage}</div>
+            : allPosts.map((post) => {
+                return <div className="post-container" key={post._id}>
+                        {post.imageURL ? <img src={post.imageURL} /> : <div className="image-placeholder2"/>}
+                        <h3>{post.title}</h3>
+                        <p>{`${post.content[0].slice(0, 150)}...`}</p>
+                        <Link to={post._id}>
+                            <DefaultButton btnType="button">Read more</DefaultButton>
+                        </Link>
+                </div>
+            })
+            }
+        </div>}
         </>
     )
 }
